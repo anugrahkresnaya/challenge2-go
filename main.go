fa3a595165e7654432dea620caa5fe7b1e7725c5
Anugrah Kresnaya <anugrahkresnaya.ak@gmail.com>
2023-03-09 22:37:45 +0700

    refactor: change the combination of loops and conditions

--- main.go
+++ main.go
@@ -3,17 +3,19 @@ package main
 import "fmt"
 
 func main()  {
-  for i := 0; i < 5; i++ {
-    fmt.Println("Nilai i =", i)
-  }
-
-  for j := 0; j <= 10; j++ {
-    if j == 5 {
-      for i, rune := range "САШАРВО" {
-        fmt.Printf("character %#U starts at byte position %d\n", rune, i)
+  for i := 0; i <= 5; i++ {
+    if i == 5 {
+      for j := 0; j <= 10; j++ {
+        if j == 5 {
+          for i, rune := range "САШАРВО" {
+            fmt.Printf("character %#U starts at byte position %d\n", rune, i)
+          }
+          continue
+        }
+        fmt.Println("Nilai j =", j)
       }
       continue
     }
-    fmt.Println("Nilai j =", j)
+    fmt.Println("Nilai i =", i)
   }
 }
